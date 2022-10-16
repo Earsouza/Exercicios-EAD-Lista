be41c1df7f2dabd96340111ef1de0d13a959d61f
Earsouza <eduardo.ar2001@aluno.ifsc.edu.br>
2022-10-15 23:58:46 -0300

    exercicios

--- src/exercicios/Exe11.java
+++ src/exercicios/Exe11.java
@@ -1,10 +1,35 @@
 package exercicios;
 
+import java.util.LinkedList;
+
 public class Exe11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int pares = 0;
+		int maior = 0;
+		LinkedList<Integer> lista = new LinkedList<Integer>();
+		lista.add(5);
+		lista.add(4);
+		lista.add(3);
+		lista.add(2);
+		lista.add(1);
 
+		for (Integer integer : lista) {
+
+			if (integer > maior)
+				maior = integer;
+
+		}
+		System.out.println("Maior valor: " + maior);
+
+		System.out.printf("Valores pares: ");
+		for (Integer integer : lista) {
+			if (integer % 2 == 0) {
+				pares = integer;
+				System.out.printf("%d ",pares);
+
+			}
+
+		}
 	}
-
 }
